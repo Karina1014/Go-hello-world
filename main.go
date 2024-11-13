ae572d27af928c7fa7444fd6b00192fdf34c0600
Karina1014 <janneth_wi15@hotmail.com>
2024-11-13 04:56:10 -0500

    feat: :pencil2: update Go file with web server functionality
    Fixed an issue where the web server wasn't responding correctly to HTTP requests on port 8080.

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, world!")
+}
+
 func main() {
-	fmt.Println("Hello, world!")
+	http.HandleFunc("/", handler)
+	port := ":8080"
+	fmt.Printf("Server running on port %s...\n", port)
+
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
+	}
 }
